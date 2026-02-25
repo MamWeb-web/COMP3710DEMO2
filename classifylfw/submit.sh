c0e558e359c943f4b57497402938b4db1f59114e
mamweb-web <mamlim675@gmail.com>
2026-02-25 15:41:55 +0800

    added submit script for lfw

---
+++ classifylfw/submit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=s4754411
+#SBATCH --partition=a100
+#SBATCH --nodes=1
+#SBATCH --time=1:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --output=slurm_logs/job_%j.out
+#SBATCH --error=slurm_logs/job_%j.error
+
+
+conda activate comp3710 
+
+time python training.py
